5da4814d42ec3a7ac039ca6b3be6683d6712c316
Aniol Martí <aniol.me@gmail.com>
2017-08-11 20:38:58 +0200

    Added connect() function

--- auth.php
+++ auth.php
@@ -11,6 +11,19 @@ define("HOST", "chase.nnbox.org");
 define("PORT", 389);
 define("PEOPLE", "ou=people,dc=nnbox,dc=org");
 
+function connect() {
+    // connect
+    $ldap = ldap_connect(HOST, PORT);
+
+    // configure ldap params
+    ldap_set_option($ldap,LDAP_OPT_PROTOCOL_VERSION,3);
+    ldap_set_option($ldap,LDAP_OPT_REFERRALS,0);
+    ldap_start_tls($ldap);
+
+    return $ldap;
+
+}
+
 function login($user, $password) {
     // check if a user and password are provided
     if(empty($user) || empty($password)) {
@@ -23,25 +36,19 @@ function login($user, $password) {
         $user = $split[0];
     }
 
-    // connect
-    $ldap_user = "cn=".$user.",ou=people,dc=nnbox,dc=org";
-    $ldap = ldap_connect(HOST, PORT);
-
-    // configure ldap params
-    ldap_set_option($ldap,LDAP_OPT_PROTOCOL_VERSION,3);
-    ldap_set_option($ldap,LDAP_OPT_REFERRALS,0);
-    ldap_start_tls($ldap);
-
     // bind
-    $bind = @ldap_bind($ldap, $ldap_user, $password);
+    $ldap = connect();
+    $ldap_user = "cn=".$user.",ou=people,dc=nnbox,dc=org";
+    $bind = ldap_bind($ldap, $ldap_user, $password);
 
     if ($bind) {
         echo "true";
-        @ldap_close($ldap);
+        ldap_close($ldap);
+
     }
     else {
         echo "Couldn't bind to the LDAP server.";
-        @ldap_close($ldap);
+        ldap_close($ldap);
     }
 }
 
@@ -58,19 +65,11 @@ function signup($user, $password) {
     }
 
     // admin user
+    $ldap = connect();
     $ldap_manager = "uid=manager,ou=services,dc=nnbox,dc=org";
     $ldap_password = "";
 
-    // connect
-    $ldap = ldap_connect(HOST, PORT);
-
-    // configure ldap params
-    ldap_set_option($ldap,LDAP_OPT_PROTOCOL_VERSION,3);
-    ldap_set_option($ldap,LDAP_OPT_REFERRALS,0);
-    ldap_start_tls($ldap);
-
-    // bind
-    $bind = @ldap_bind($ldap, $ldap_manager, $ldap_password);
+    $bind = ldap_bind($ldap, $ldap_manager, $ldap_password);
 
     if ($bind) {
         // we check if the user exists
@@ -105,11 +104,11 @@ function signup($user, $password) {
             exit('This user already exists.');
         }
 
-        @ldap_close($ldap);
+        ldap_close($ldap);
     }
     else {
         echo "Couldn't bind to the LDAP server.";
-        @ldap_close($ldap);
+        ldap_close($ldap);
     }
 }
 
