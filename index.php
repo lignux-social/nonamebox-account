74bf38bfafee2be9e2dbf75a11254d760fa60369
Agustí Montes <a.gus493@gmail.com>
2017-08-11 18:41:25 +0200

    Header, footer, sidebar and home view done.

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Account Management | NoNameBox</title>
+    <link href="/assets/bootstrap/css/bootrstrap.min.css" type="text/css" rel="stylesheet" />
+    <link href="/assets/big-picture/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
+    <link href="/assets/big-picture/css/main.css" type="text/css" rel="stylesheet" />
+    <link href="/assets/big-picture/css/nnb_custom.css" type="text/css" rel="stylesheet" />
+
+  </head>
+  <body>
+    <header id="header">
+      <h1 id="logo"><a href="/"> <img src="/assets/img/gus_nnblogo.png" alt="No Name Box" class="header-logo"></a></h1>
+      <nav id="nav">
+          <ul>
+              <li><a href="http://nonamebox.org/">Get back to the site</a></li>
+          </ul>
+      </nav>
+    </header>
+
+    <div class="container">
+      <div class="col-md-4">
+        <ul>
+          <li><h3>TEST</h3></li>
+          <li><h3>TEST</h3></li>
+          <li><h3>TEST</h3></li>
+        </ul>
+      </div>
+      <div class="col-md-8">
+        <h2>This is a test</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+
+    <footer id="footer">
+      <ul class="actions">
+          <li><a href="https://t.me/NoNameBox" class="icon fa-telegram"></a></li>
+          <li><a href="mailto:info@nonamebox.org" class="icon fa-envelope"></a></li>
+      </ul>
+    </footer>
+
+    <script src="/assets/jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
+    <script src="/assets/big-picture/js/jquery.poptrox.min.js" type="text/javascript" ></script>
+    <script src="/assets/big-picture/js/jquery.scrolly.min.js" type="text/javascript" ></script>
+    <script src="/assets/big-picture/js/jquery.scrollex.min.js" type="text/javascript" ></script>
+    <script src="/assets/big-picture/js/skel.min.js" type="text/javascript" ></script>
+    <script src="/assets/big-picture/js/util.js" type="text/javascript" ></script>
+    <script src="/assets/big-picture/js/main.js" type="text/javascript" ></script>
+  </body>
+</html>
